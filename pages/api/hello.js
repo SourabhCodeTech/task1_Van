491b72521585dba998eb72694a970ec2ddedf7f3
Sourabh Gautam <83059897+SourabhCodeTech@users.noreply.github.com>
2023-05-24 19:25:18 +0530

    First Commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,48 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(
+    [
+
+      {
+
+        "name": "Justice Campos",
+
+        "points": 23,
+
+        "avatar": "https://i.pravatar.cc/300"
+
+      },
+
+      {
+
+        "name": "Miranda Carpenter",
+
+        "points": 32,
+
+        "avatar": "https://i.pravatar.cc/300"
+
+      },
+
+      {
+
+        "name": "Kailyn Estes",
+
+        "points": 34,
+
+        "avatar": "https://i.pravatar.cc/300"
+
+      },
+
+      {
+
+        "name": "Iliana Carson",
+
+        "points": 12,
+
+        "avatar": "https://i.pravatar.cc/300"
+
+      }
+    ]
+  )
 }
